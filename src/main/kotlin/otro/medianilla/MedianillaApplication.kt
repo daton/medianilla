dfe294b3d8dabadf57971a93f988eddadf34916e
daton <rapidclimate@gmail.com>
2021-10-01 12:18:14 -0500

    Initial commit

---
+++ src/main/kotlin/otro/medianilla/MedianillaApplication.kt
@@ -0,0 +1,11 @@
+package otro.medianilla
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MedianillaApplication
+
+fun main(args: Array<String>) {
+	runApplication<MedianillaApplication>(*args)
+}
---
+++ src/test/kotlin/otro/medianilla/MedianillaApplicationTests.kt
@@ -0,0 +1,13 @@
+package otro.medianilla
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MedianillaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
